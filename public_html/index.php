9c612b5eca4f3acf1615edf9d8c34e9d339f80df
Vince de Jong <vincedejong@hotmail.com>
2020-05-13 17:05:19 +0200

    Tried making a template enigine, didnt work : ^)

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,22 @@
 require_once('../config/config.php');
 require_once('../includes/library.php');
 
+// Require classes
+require_once('../includes/TemplateHandler.class.php');
 
+$TemplateHandler = new TemplateHandler();
+
+$y = 'yeet';
+
+$x = array();
+$x['one'] = 'yeet';
+$x['two'] = 'test';
+$x['four'] = '4';
+$x['five'] = '5';
+
+$TemplateHandler->assign('test', 	$x);
+$TemplateHandler->assign('tester', 	$y);
+
+$TemplateHandler->render('home');
 
  ?>
---
+++ public_html/templates/home.tpl.php
@@ -0,0 +1,11 @@
+test.one => yeet: {$test.one}
+<br><br>
+test.two => test: {$test.two}
+<br><br>
+test.three.one => uhm.. : {$test.three.nine}
+<br><br>
+{$test.four}
+<br><br>
+{$test.five}
+<br><br>
+{$tester}
